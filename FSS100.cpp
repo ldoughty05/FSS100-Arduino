febae18b768038ed3b3ad548f2ce52f67889e61d
Walker Arce <wsarcera@gmail.com>
2023-11-29 13:41:57 -0600

    Update library commands
    - Improve CONL handling
    - Improve one shot sampling
    - Add get address

--- FSS100.cpp
+++ FSS100.cpp
@@ -2,8 +2,6 @@
 #include "FSS100.h"
 #include <Wire.h>
 
-// Find some way to periodically set control register.
-
 FSS100::FSS100(int address)
 {
 	_addr = address;
@@ -12,7 +10,6 @@ FSS100::FSS100(int address)
 
 void FSS100::init()
 {
-	//Wire.begin();
 	default_config();
 }
 
@@ -21,20 +18,30 @@ void FSS100::default_config(void)
 	setContinuousSampling(false);
 	setSamplingRate(FSS100_SR_16HZ);
 	setSampleBit(true);
-	update_conl();
+	uint8_t current_conl = get_conl();
+	if (current_conl != _conl_reg)  update_conl();
 }
 
 void FSS100::continuous_config(void)
 {
 	setContinuousSampling(true);
 	setSamplingRate(FSS100_SR_16HZ);
+	setSampleBit(false);
+	uint8_t current_conl = get_conl();
+	if (current_conl != _conl_reg) update_conl();
+}
+
+void FSS100::set_one_shot(void)
+{
+	set_conl(get_conl());
+	setSampleBit(true);
 	update_conl();
 }
 
 bool FSS100::sample_wait()
 {
 	Wire.beginTransmission(_addr);
-	Wire.write(0x0A);
+	Wire.write(0x08);
 	Wire.endTransmission();
 	Wire.requestFrom(_addr, 1);
 	if (Wire.available())
@@ -52,18 +59,19 @@ bool FSS100::sample_wait()
 	return false;
 }
 
-void FSS100::getSample(int16_t *theta, int16_t *phi)
+void FSS100::getSample(int16_t *theta, int16_t *phi, int16_t *temp)
 {
 	Wire.beginTransmission(_addr);
 	Wire.write(0x00);
 	Wire.endTransmission();
 
-	uint16_t theta_out = 0;
-	uint16_t phi_out = 0;
+	int16_t theta_out = 0;
+	int16_t phi_out = 0;
+	int16_t temp_out = 0;
 
 	int i = 0;
 
-	Wire.requestFrom(_addr, 4);
+	Wire.requestFrom(_addr, 6);
 	while(Wire.available())
 	{
 		char c = Wire.read();
@@ -83,11 +91,20 @@ void FSS100::getSample(int16_t *theta, int16_t *phi)
 		{
 			phi_out |= (c << 8);
 		}
+		else if (i == 4)
+		{
+			temp_out = c;
+		}
+		else if (i == 5)
+		{
+			temp_out |= (c << 8);
+		}
 
 		i++;
 	}
 	*theta = theta_out;
 	*phi = phi_out;
+	*temp = temp_out;
 }
 
 int16_t FSS100::getTheta() {
@@ -141,6 +158,32 @@ int16_t FSS100::getPhi()
 	return phi;
 }
 
+int16_t FSS100::getTemp()
+{
+	Wire.beginTransmission(_addr);
+	Wire.write(0x04);
+	Wire.endTransmission();
+
+	int16_t temp = 0;
+	int i = 0;
+	Wire.requestFrom(_addr, 2);
+	while(Wire.available())
+	{
+		char c = Wire.read();
+		if (i == 0)
+		{
+			temp |= c;
+		}
+		else if (i == 1)
+		{
+			temp |= (c << 8);
+		}
+		i++;
+	}
+
+	return temp;
+}
+
 void FSS100::setI2CAddress(int newAddress)
 {
 	Wire.beginTransmission(_addr);
@@ -151,12 +194,51 @@ void FSS100::setI2CAddress(int newAddress)
 	_addr = newAddress;
 }
 
+uint8_t FSS100::get_conl(void)
+{
+	Wire.beginTransmission(_addr);
+	Wire.write(0x0A);
+	Wire.endTransmission();
+
+	uint8_t conl_reg = 0;
+	int i = 0;
+	Wire.requestFrom(_addr, 1);
+	while(Wire.available())
+	{
+		conl_reg = Wire.read();
+	}
+
+	return conl_reg;
+}
+
 void FSS100::update_conl(void)
 {
 	Wire.beginTransmission(_addr);
 	Wire.write(0x0A);
 	Wire.write(_conl_reg);
 	Wire.endTransmission();
+}
+
+void FSS100::set_conl(uint8_t new_conl)
+{
+	_conl_reg = new_conl;
+}
+
+uint8_t FSS100::getAddress(void)
+{
+	Wire.beginTransmission(_addr);
+	Wire.write(0x0B);
+	Wire.endTransmission();
+
+	uint8_t addr_reg = 0;
+	int i = 0;
+	Wire.requestFrom(_addr, 1);
+	while(Wire.available())
+	{
+		addr_reg = Wire.read();
+	}
+
+	return addr_reg;
 }
 
 void FSS100::setContinuousSampling(bool continuous)
@@ -167,7 +249,14 @@ void FSS100::setContinuousSampling(bool continuous)
 
 void FSS100::setSamplingRate(int samplingRate)
 {
+	_conl_reg &= FSS100_SR_CLEAR;
 	_conl_reg |= samplingRate;
+}
+
+void FSS100::setCommitBit(bool commit)
+{
+	if (commit) _conl_reg |= FSS100_CONL_COMMIT;
+	else _conl_reg &= FSS100_CONL_COMMIT_CLEAR;
 }
 
 void FSS100::setSampleBit(bool sample)
--- FSS100.h
+++ FSS100.h
@@ -3,12 +3,18 @@
 
 #include "Energia.h"
 
+#define FSS100_SR_CLEAR ~((1 << 4) | (1 << 3))
 #define FSS100_SR_32HZ (1 << 4) | (1 << 3)
 #define FSS100_SR_16HZ (1 << 4) | (0 << 3)
 #define FSS100_SR_8HZ (0 << 4) | (1 << 3)
 #define FSS100_SR_4HZ (0 << 4) | (0 << 3)
+
+#define FSS100_CONL_COMMIT (1 << 5)
+#define FSS100_CONL_COMMIT_CLEAR ~FSS100_CONL_COMMIT
+
 #define FSS100_CONL_CONTINUOUS (1 << 6)
 #define FSS100_CONL_ONESHOT ~FSS100_CONL_CONTINUOUS
+
 #define FSS100_CONL_SAMPLEREQUEST (1 << 7)
 #define FSS100_CONL_SAMPLEDONE ~FSS100_CONL_SAMPLEREQUEST
 
@@ -17,15 +23,21 @@ class FSS100
 	public:
 	FSS100(int address);
 	void init();
-	void getSample(int16_t *theta, int16_t *phi);
+	void getSample(int16_t *theta, int16_t *phi, int16_t *temp);
 	int16_t getTheta();
 	int16_t getPhi();
+	int16_t getTemp();
+	uint8_t get_conl(void);
+	void set_conl(uint8_t new_conl);
+	void set_one_shot(void);
+	uint8_t getAddress(void);
 	void setI2CAddress(int address);
 	void update_conl();
 	void setContinuousSampling(bool continuous);
 	void setSamplingRate(int rate);
 	bool sample_wait(void);
 	void setSampleBit(bool sample);
+	void setCommitBit(bool commit);
 	void default_config(void);
 	void continuous_config(void);
 
